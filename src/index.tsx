42e455274b6f02f9dd33303880065d39c0d82845
ychae1997 <77264092+ychae1997@users.noreply.github.com>
2023-09-08 21:26:02 +0900

    chore : install ts, tailwind

--- src/index.tsx
+++ src/index.tsx
@@ -40,7 +40,8 @@ const router = createBrowserRouter([
   }
 ])
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+
 root.render(
   <React.StrictMode>
     <RouterProvider router={router} />
