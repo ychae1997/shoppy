cbe1217a5889dddc13f0f6af4f470cb7face3ac1
ychae1997 <77264092+ychae1997@users.noreply.github.com>
2023-09-14 03:31:15 +0900

    feat : button type 추가

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,9 +1,10 @@
 import { ButtonType } from "../../types/elementTypes";
 
-export default function Button({ text, onClick }: ButtonType) {
+export default function Button({ text, onClick, type = "button" }: ButtonType) {
   return (
     <button
       className="bg-brand text-white px-4 py-2 rounded-md hover:brightness-110"
+      type={type}
       onClick={onClick}>
       {text}
     </button>
