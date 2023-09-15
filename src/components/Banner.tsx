79fab7301c34cd146b791f7f43f89dab1da87ea2
ychae1997 <77264092+ychae1997@users.noreply.github.com>
2023-09-16 04:36:47 +0900

    feat : 메인페이지 배너 추가

---
+++ src/components/Banner.tsx
@@ -0,0 +1,12 @@
+export default function Banner() {
+  return (
+    <section className="h-96  relative">
+      <div className="w-full h-full bg-cover bg-banner">
+        <div className="flex flex-col h-full justify-center items-center text-white bg-gray-500/30">
+          <h2 className="text-5xl mb-2">Shop With Us</h2>
+          <p className="text-2xl">Best Products, High Quality</p>
+        </div>
+      </div>
+    </section>
+  );
+}
